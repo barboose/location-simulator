8c90ffd4b54e3589cf7f10755780241d34ca73b6
barboose <barna.varga@tuta.io>
2020-07-11 22:14:48 +0200

    Initial commit

---
+++ src/main/java/com/barnavarga/locationsimulator/LocationSimulatorApplication.java
@@ -0,0 +1,15 @@
+package com.barnavarga.locationsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSimulatorApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LocationSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barnavarga/locationsimulator/LocationSimulatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.barnavarga.locationsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSimulatorApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
